82bcac90d776cd639db0f67bd32c775913fc6baf
hylim <gywjd8938@naver.com>
2022-12-24 00:04:13 +0900

    Docs: const와 let의 차이

--- momentum/script.js
+++ momentum/script.js
@@ -1,8 +1,17 @@
 const a = 5;
 const b = 2;
-const myName = "hyojeong";
+let myName = "hyojeong";
+/*
+[const 와 let의 차이점]
+const(constant상수) - 값이 바뀔 수 없음
+let - 값이 변경될 수 있음
+*/
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "gywjd";
+
+console.log("your new name is " + myName);
