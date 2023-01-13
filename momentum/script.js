dbf5201880654aa1015dff29f37cd106c9b4b6f6
hylim <gywjd8938@naver.com>
2023-01-13 23:40:18 +0900

    Feature: 사용자이름을 입력하면 form이 사라지고 사용자이름이 화면에 출력됨

--- momentum/script.js
+++ momentum/script.js
@@ -1,34 +1,25 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+// 일반적으로 string만 포함된 변수는 대문자로 표기하고, string을 저장하고 싶을 때 사용
 
 function onLoginSubmit(event) {
   // event object를 argument로 줌
   event.preventDefault(); // 어떤 event의 기본 행동(브라우저가 기본적으로 수행하는 동작)이든 발생되지 않도록 막는 function
-  //const userName = loginInput.value;
-  //console.log(event);
-  console.log(loginInput.value);
-}
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const userName = loginInput.value;
 
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.log(event);
-  console.dir(event);
-}
+  greeting.innerHTML = `Hello ${userName}`;
+  // greeting.innerHTML = "Hello " + userName; 위와 동일한 코드
+  // '' 와 "" 이 아닌 ``(백틱)을 사용해야 함
+  // 변수와 string을 결합하고 싶다면 또는 변수를 string안에 넣고 싶다면 ${변수명} 이렇게 표현하면 됨
 
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
 
-// 뭐가 클릭됐는지 어디가 클릭됐는지 정보를 알고 싶을 때 js는 단순히 함수를 실행시키기만 하는 것이 아니라
-// js는 함수를 실행시키는 동시에 그 함수에 첫번째 인자로 object를 넣어줌
-//handleLinkClick({information about the event that just happend});
-// 이 object에는 방금 일어난 event에 대한 여러 정보가 담겨있음
-// 방금 일어난 event에 대한 정보를 담은 이 object는 handleLinkClick을 위한 EventListener 함수의 첫번째 인자로 주어지게 됨
-
-// 배운 내용 정리
-// 브라우저는 링크를 클릭할 때 해당 사이트로 이동하도록 설정이 되어있는데 그 기본 동작을 막음
-// EventListener 안에 있는 함수는 내가 직접 실행하지 않고 브라우저가 해줌.
-// 브라우저는 실행만 해주는 것이 아니라 event에 대한 정보도 담아줌.
-// 우리는 자리만 만들어주면 되고, 이 정보안에는 몇가지 함수도 같이 들어있음
-// ㄴ ex) preventDefault 이 함수를 호출하게 되면 event(라는 이름의 argument)는 멈추게 되고 아무것도 진행하지 않음
+// 배운 내용
+// 백틱을 사용하여 변수와 string 결합 방법 새로운 방법을 알게 됨 ``, ${변수명} 규칙이 있음
+// class hidden을 사용하여 input을 숨겼다가 h1을 보이게 하는 방법 등을 할 수 있음
