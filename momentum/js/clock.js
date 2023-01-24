be3804dcff68d930aa942bc45942840af657d9ff
hylim <gywjd8938@naver.com>
2023-01-24 22:50:58 +0900

    Feature: 1초마다 시간,분,초가 발생되는 로직

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -10,9 +10,35 @@ setInterval()
  두번째 argument는 호출되는 function 간격을 몇 ms로 할 지 쓰면 됨
  1000ms = 1s(초)
 */
+//setInterval(sayHello, 5000);
+
+/*
+timeout : 일정시간이 흐른 뒤 실행되는 법
+ ㄴ ex) '5초 뒤'
+
+setTimeout()
+호출하고자 하는 function, 얼마나 기다릴지 ms단위 입력
+*/
+//setTimeout(sayHello, 5000);
 
 function sayHello() {
   console.log("Hello");
 }
 
-setInterval(sayHello, 5000);
+/*
+const date = new Date(); 
+
+date.getDate(); // 며칠
+date.getDay(); // 무슨요일
+date.getFullYear() // 몇년
+date.getHours(); // 몇시
+date.getMinutes(); // 몇분
+date.getSeconds(); // 몇초
+*/
+
+function sayClock() {
+  const date = new Date();
+  console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+}
+
+setInterval(sayClock, 1000);
