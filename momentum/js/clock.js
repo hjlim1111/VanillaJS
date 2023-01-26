3c98741ca654bab02ed45d79d8c42d678928c5ba
hylim <gywjd8938@naver.com>
2023-01-26 22:18:53 +0900

    Feature: clock 만들기. padStart를 활용하여 00으로 표시되게 로직구현

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -38,7 +38,34 @@ date.getSeconds(); // 몇초
 
 function sayClock() {
   const date = new Date();
-  console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+sayClock();
 setInterval(sayClock, 1000);
+
+/*
+1 이 아닌 01 로 표시하고 싶을 때
+padStart : 앞에 추가
+padEnd : 뒤에 추가
+ ㄴ string에 쓸 수 있는 function임
+
+ex)
+string.padStart(string의 길이, 추가할 숫자);
+"1".padStart(2,"0"); // 결과값 01
+"12".padStart(2,"0") // 결과값 12 아무런 변화가 일어나지 않음
+
+"1".padEnd(2,"0") // 결과값 10
+*/
+
+/*
+new Date().getHours(); //결과값이 number
+number에서 string으로 변환하려면?
+String으로 감싸주면 됨
+
+String(new Date().getHours());
+*/
